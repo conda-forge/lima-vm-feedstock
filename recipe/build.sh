0e295fc76b486f5173357bb3dd8be41f75aaa482
danielnachun <daniel.nachun@gmail.com>
2024-07-11 10:00:32 -0700

    enable non-Intel platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,11 +12,13 @@ make VERSION=${PKG_VERSION}
 cp -r _output/bin/* ${PREFIX}/bin
 cp -r _output/share/* ${PREFIX}/share
 
-mkdir -p ${PREFIX}/etc/bash_completion.d
-mkdir -p ${PREFIX}/share/zsh/site-functions
-mkdir -p ${PREFIX}/share/fish/vendor_completions.d
-limactl completion bash > ${PREFIX}/etc/bash_completion.d/lima
-limactl completion zsh > ${PREFIX}/share/zsh/site-functions/_lima
-limactl completion fish > ${PREFIX}/share/fish/vendor_completions.d/lima.fish
+if [[ ${target_platform} == ${build_platform} ]]; then
+    mkdir -p ${PREFIX}/etc/bash_completion.d
+    mkdir -p ${PREFIX}/share/zsh/site-functions
+    mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+    limactl completion bash > ${PREFIX}/etc/bash_completion.d/lima
+    limactl completion zsh > ${PREFIX}/share/zsh/site-functions/_lima
+    limactl completion fish > ${PREFIX}/share/fish/vendor_completions.d/lima.fish
+fi
 
 go-licenses save ./cmd/limactl --save_path=license-files
