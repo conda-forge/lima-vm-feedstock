cd7b07f2f8b92bc6977e7de3194775122d097ba8
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-24 22:36:59 -0800

    more fixes

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,8 +1,10 @@
 mkdir %LIBRARY_PREFIX%\bin
 mkdir %LIBRARY_PREFIX%\share
 
-rm pkg\cidata\cloud-config.yaml || goto :error
-copy pkg\cidata\cidata.TEMPLATE.d\user-data pkg\cidata\cloud-config.yaml || goto :error
+REM rm pkg\cidata\cloud-config.yaml || goto :error
+REM rm pkg\limayaml\default.yaml || goto :error
+REM copy pkg\cidata\cidata.TEMPLATE.d\user-data pkg\cidata\cloud-config.yaml || goto :error
+REM copy pkg\templates\ pkg\limayaml\default.yaml || goto :error
 make VERSION=%PKG_VERSION% || goto :error
 xcopy _output\bin\* %LIBRARY_PREFIX%\bin || goto :error
 xcopy _output\share\* %LIBRARY_PREFIX%\share || goto :error
