59967a984f40214179c9d75411095760c2e45918
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-24 22:49:22 -0800

    more fixes

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,17 +1,17 @@
+@echo on
+@setlocal EnableDelayedExpansion
+
 mkdir %LIBRARY_PREFIX%\bin
 mkdir %LIBRARY_PREFIX%\share
 
-REM rm pkg\cidata\cloud-config.yaml || goto :error
-REM rm pkg\limayaml\default.yaml || goto :error
-REM copy pkg\cidata\cidata.TEMPLATE.d\user-data pkg\cidata\cloud-config.yaml || goto :error
-REM copy pkg\templates\ pkg\limayaml\default.yaml || goto :error
+set GOFLAGS="-modcacherw -trimpath"
 make VERSION=%PKG_VERSION% || goto :error
 xcopy _output\bin\* %LIBRARY_PREFIX%\bin || goto :error
 xcopy _output\share\* %LIBRARY_PREFIX%\share || goto :error
 
 go-licenses save .\cmd\limactl --save_path=license-files || goto :error
 
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
